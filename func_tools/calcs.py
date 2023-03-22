1cc3685f8e3b9bcab922ea4e1adb4ef065d0db2c
Alla_Dseshulska <dzeshulska@gmail.com>
2023-03-22 23:28:58 +0200

    changed

--- func_tools/calcs.py
+++ func_tools/calcs.py
@@ -1,27 +1,24 @@
-from func_tools.divide import divide 
-from func_tools.sum import sum
-from func_tools.subtruct import subtruct
-from func_tools.multiply import multiply
-
-
-def calcs():
-    operation = input("""
-    Please type in the match operation you would like to complete :
-    + for summation
-    - for subtraction
-    * for multiplication
-    / for division""")
-
-while is_running:
-    number_1 = int(input("Enter first number!"))
-    number_2 = int(input("Enter second number!"))
-
-    if operation == "+": 
-        func_tools.sum(a, b)
-    elif operation == "*": 
-        func_tools.multiply(a, b)
-    elif operation == "/": 
-        func_tools.divide(a, b)
+def calculator(a,b,operation):
+    if operation == "+":
+        result = a + b
+    elif operation == "-":
+        result = a - b
+    elif operation == "*":
+        result = a * b
+    elif operation == "/":
+        result = a/b
     else:
-        print("you have not typed a valid operator, please run the program again")
-        calculate()
+        print("Unsupportet operation")
+    if result is not None:
+        print("Result: ", result)
+
+a = int(input("First number: "))
+
+b = int(input("Second number: "))
+
+operation = input("Operation: ")
+
+calculator(a,b,operation)
+
+
+
--- func_tools/calcs2.py
+++ func_tools/calcs2.py
@@ -1,9 +1,13 @@
-from divide import divide
-from sum import sum
-from subtruct import subtruct
-from multiply import multiply
 
 
+a = int(input("Enter first number!"))
+b = int(input("Enter second number!"))
+operation = input("""
+        Please type in the match operation you would like to complete :
+        + for summation
+        - for subtraction
+        * for multiplication
+        / for division""")
 def calcs(a,b,operation):
     if operation == "+": 
          sum.sum(a, b)
@@ -15,13 +19,5 @@ def calcs(a,b,operation):
         divide.divide(a, b)
     else:
         print("you have not typed a valid operator, please run the program again")
-a = int(input("Enter first number!"))
-b = int(input("Enter second number!"))
-operation = input("""
-        Please type in the match operation you would like to complete :
-        + for summation
-        - for subtraction
-        * for multiplication
-        / for division""")
 
 calcs(a,b,operation)
